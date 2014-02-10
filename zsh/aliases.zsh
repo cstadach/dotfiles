ecac099dbdb6fe8193653b8038a2417945961f36
Christian Stadach <christian@stadach.com>
2014-02-10 13:41:58 +0100

    changed standard editor and added juniper vpn start script

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,9 @@
+#################################################################################
+# start and stop the vpn from the command line from now on with these two commands
+# or rename the aliases as you see fit.
+# https://gist.github.com/Andrewpk/7558715
+#################################################################################
+alias startpulsevpn="sudo launchctl load -w /Library/LaunchDaemons/net.juniper.AccessService.plist; open -a '/Applications/Junos Pulse.app/Contents/Plugins/JamUI/PulseTray.app/Contents/MacOS/PulseTray'"
+alias quitpulsevpn="osascript -e 'tell application \"PulseTray.app\" to quit';sudo launchctl unload -w /Library/LaunchDaemons/net.juniper.AccessService.plist"
+
 alias reload!='. ~/.zshrc'
