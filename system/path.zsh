ae6183d21a61e82059448f7f9a03237d85bfe5eb
Christian Stadach <christian.stadach@otto.de>
2020-06-19 17:15:18 +0200

    introducing local bin directory

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="$HOME/.rbenv/shims:./bin:$HOME/.rbenv/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$HOME/.local/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
