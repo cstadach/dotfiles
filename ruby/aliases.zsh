7d080325b59d1e6bdf11d3deb06ad4b326000c02
Christian Stadach <christian.stadach@otto.de>
2020-06-19 17:14:19 +0200

    removed ruby settings

--- ruby/aliases.zsh
+++
@@ -1,8 +0,0 @@
-alias r='rbenv local 1.8.7-p374'
-
-alias sv='script/server'
-alias sc='script/console'
-alias sg='script/generate'
-alias sd='script/destroy'
-
-alias migrate='rake db:migrate db:test:clone'
