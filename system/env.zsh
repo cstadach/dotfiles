30af300f164d98439ef165316369720c93ed2a5b
Christian Stadach <christian.stadach@otto.de>
2020-10-06 10:37:34 +0200

    add display exports for wsl2

--- system/env.zsh
+++ system/env.zsh
@@ -3,3 +3,8 @@ if [[ "$EDITOR" == "" ]] ; then
   # Use vim for my editor.
   export EDITOR='vim'
 fi
+# add display for wsl 2
+if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+  export DISPLAY=$(ip route | awk '{print $3; exit}'):0
+  export LIBGL_ALWAYS_INDIRECT=1
+fi
