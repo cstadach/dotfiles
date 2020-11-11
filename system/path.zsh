b76c8731a6a5f4ee99a3317357fc4faddbbb3c83
Christian Stadach <christian.stadach@otto.de>
2020-11-11 11:08:08 +0100

    added snap path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$HOME/.local/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$HOME/.local/bin:/snap/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
