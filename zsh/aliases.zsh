5b937c2b4d9e0acbb5d4090ba1cde268bce2fc10
Christian Stadach <christian.stadach@otto.de>
2021-04-14 10:45:32 +0200

    removed ancient comment

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,1 @@
-#################################################################################
-# start and stop the vpn from the command line from now on with these two commands
-# or rename the aliases as you see fit.
-# https://gist.github.com/Andrewpk/7558715
-#################################################################################
 alias reload!='. ~/.zshrc'
