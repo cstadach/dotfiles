d19a01d23ee3ea39923dad176904f5e30b8362e7
Christian Stadach <christian.stadach@otto.de>
2022-06-10 10:18:29 +0200

    added -h parameter to ls alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,8 +3,8 @@
 #   `brew install coreutils`
 case $( uname -s ) in
   Linux)
-    alias ls="ls -F --color"
-    alias l="ls -lAh --color"
+    alias ls="ls -Fh --color"
+    alias l="ls -lA --color"
     alias ll="ls -l --color"
     alias la='ls -A --color'
     ;;
@@ -12,10 +12,10 @@ case $( uname -s ) in
     alias uuid="uuidgen | tr -d '\n' | tr '[:upper:]' '[:lower:]'  | pbcopy && pbpaste && echo"
     if $(gls &>/dev/null)
     then
-      alias ls="gls -F --color"
-      alias l="gls -lAh --color"
-      alias ll="gls -l --color"
-      alias la='gls -A --color'
+      alias ls="gls -Fh --color"
+      alias l="ls -lAh --color"
+      alias ll="ls -l --color"
+      alias la='ls -A --color'
     fi
     alias tar="gtar"
     ;;
