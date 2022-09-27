14c75d19acf860e89bab171cc134cf50dea16ba7
Christian Stadach <christian.stadach@otto.de>
2022-09-27 10:55:21 +0200

    removed async plugin

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,5 +1,4 @@
 export ZPLUG_HOME=/opt/homebrew/opt/zplug
 source $ZPLUG_HOME/init.zsh
-zplug "mafredri/zsh-async", from:"github", use:"async.zsh"
 zplug "spaceship-prompt/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
 zplug load
