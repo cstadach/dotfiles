19acc898127d4a736ad08fd582b0974978fef79a
Christian Stadach <christian.stadach@otto.de>
2023-03-21 09:52:54 +0200

    added new versions.tf directive

--- terraform/load_tfswitch.zsh
+++ terraform/load_tfswitch.zsh
@@ -1,9 +1,13 @@
 autoload -U add-zsh-hook
 load-tfswitch() {
-  local tfswitchrc_path=".tfswitchrc" local version_tf_path="versions.tf"
+  local tfswitchrc_path=".tfswitchrc"
+  local version_tf_path="version.tf"
+  local versions_tf_path="versions.tf"
   if [ -f "$tfswitchrc_path" ]; then
     tfswitch
   elif [ -f "$version_tf_path" ]; then
+    tfswitch
+  elif [ -f "$versions_tf_path" ]; then
     tfswitch
   else
     tfswitch --latest >/dev/null 2>&1
