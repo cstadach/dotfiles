1842afea45ecb1f1a787d54b813d8574fe74ddda
Christian Stadach <christian.stadach@otto.de>
2023-05-11 13:26:07 +0200

    changed prompt to p10k

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -3,14 +3,4 @@ ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
 
-zinit light spaceship-prompt/spaceship-prompt
-# zplug "zsh-users/zsh-completions"
-#
-# Plugin history-search-multi-word loaded with investigating.
-zinit light zdharma-continuum/history-search-multi-word
-zinit light zsh-users/zsh-completions
-
-# Two regular plugins loaded without investigating.
-zinit light zsh-users/zsh-autosuggestions
-zinit light zdharma-continuum/fast-syntax-highlighting
-
+zinit ice depth=1; zinit light romkatv/powerlevel10k
