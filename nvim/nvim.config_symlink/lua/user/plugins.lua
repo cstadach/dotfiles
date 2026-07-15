9d7cb728427bfced27e10f8bbf6575d2af8af33a
Christian <christian@stadach.com>
2026-07-15 14:32:06 +0300

    refactor(plugins): lazy-load plugins, replace neodev with lazydev, fix CopilotChat session load

--- nvim/nvim.config_symlink/lua/user/plugins.lua
+++ nvim/nvim.config_symlink/lua/user/plugins.lua
@@ -16,23 +16,24 @@ vim.lsp.log.set_level("off")
 
 require('lazy').setup({
 
-  -- Editing utilities
+  -- Editing utilities (loaded immediately, lightweight)
   'tpope/vim-surround',
-  'tpope/vim-dispatch',
-  'tpope/vim-unimpaired',
-  'tpope/vim-abolish',
   'tpope/vim-repeat',
-  'tpope/vim-endwise',
   'tpope/vim-sleuth',
-  { 'folke/which-key.nvim', opts = {} },
-  { 'numToStr/Comment.nvim', opts = {} },
+  { 'tpope/vim-dispatch',    cmd = { 'Dispatch', 'Make', 'Focus', 'Start' } },
+  { 'tpope/vim-unimpaired',  event = 'BufReadPost' },
+  { 'tpope/vim-abolish',     event = 'BufReadPost' },
+  { 'tpope/vim-endwise',     event = 'InsertEnter' },
+  { 'folke/which-key.nvim',  event = 'VeryLazy', opts = {} },
+  { 'numToStr/Comment.nvim', event = 'BufReadPost', opts = {} },
 
   -- Git
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
-  'sindrets/diffview.nvim',
+  { 'tpope/vim-fugitive',   cmd = { 'Git', 'Gdiff', 'Gblame', 'Gpush', 'Gpull' } },
+  { 'tpope/vim-rhubarb',    dependencies = 'tpope/vim-fugitive' },
+  { 'sindrets/diffview.nvim', cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles' } },
   {
     'lewis6991/gitsigns.nvim',
+    event = 'BufReadPre',
     opts = {
       signs = {
         add          = { text = '+' },
@@ -45,9 +46,10 @@ require('lazy').setup({
   },
 
   -- Infrastructure / DevOps
-  'rhadley-recurly/vim-terragrunt',
+  { 'rhadley-recurly/vim-terragrunt', ft = { 'terragrunt', 'hcl' } },
   {
     'hashivim/vim-terraform',
+    ft = { 'terraform', 'tf', 'hcl' },
     config = function()
       vim.g.terraform_fmt_on_save = 1
     end,
@@ -56,6 +58,7 @@ require('lazy').setup({
   -- AI / Copilot
   {
     'github/copilot.vim',
+    event = 'InsertEnter',
     config = function()
       vim.g.copilot_filetypes = {
         ['*']          = false,
@@ -79,6 +82,10 @@ require('lazy').setup({
       { 'nvim-lua/plenary.nvim', branch = 'master' },
     },
     build = 'make tiktoken',
+    cmd = { 'CopilotChat', 'CopilotChatToggle' },
+    keys = {
+      { '<leader>aa', function() require('CopilotChat').toggle() end, desc = 'Toggle CopilotChat' },
+    },
     opts = {
       model = 'claude-sonnet-4.6',
     },
@@ -86,11 +93,9 @@ require('lazy').setup({
       local function get_history_path()
         local cwd = vim.fn.getcwd()
         local result = vim.fn.systemlist({ 'git', '-C', cwd, 'rev-parse', '--show-toplevel' })
-
         if vim.v.shell_error == 0 and result[1] and result[1] ~= '' and result[1] == cwd then
           return cwd
         end
-
         return nil
       end
 
@@ -103,129 +108,187 @@ require('lazy').setup({
 
       require('CopilotChat').setup(opts)
 
-      vim.api.nvim_create_autocmd('VimEnter', {
-        once = true,
-        callback = function()
-          local path = get_history_path()
-          if path then
-            local history_file = path .. '/.copilot_session.json'
-            if vim.fn.filereadable(history_file) == 1 then
-              require('CopilotChat').load('.copilot_session', path)
-            end
+      vim.schedule(function()
+        local path = get_history_path()
+        if path then
+          local history_file = path .. '/.copilot_session.json'
+          if vim.fn.filereadable(history_file) == 1 then
+            require('CopilotChat').load('.copilot_session', path)
           end
-        end,
-      })
-
-      vim.keymap.set('n', '<leader>aa', function()
-        require('CopilotChat').toggle()
-      end, { desc = 'Toggle CopilotChat' })
+        end
+      end)
     end,
   },
 
   -- LSP
   {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
-      { 'j-hui/fidget.nvim', opts = {} },
-      'folke/neodev.nvim',
-    },
-  },
-  { 'ray-x/lsp_signature.nvim', config = false },
-
-  -- Completion
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      'rafamadriz/friendly-snippets',
-    },
-  },
-
-  -- Themes
-  {
-    'EdenEast/nightfox.nvim',
-    config = function()
-      require('nightfox').setup {}
-      vim.o.background = 'dark'
-      vim.cmd.colorscheme 'nightfox'
-    end,
-  },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    config = function()
-      require('catppuccin').setup {
-        background = { light = 'latte', dark = 'mocha' },
-      }
-      -- vim.cmd.colorscheme 'catppuccin'
-    end,
-  },
-
-  -- UI
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', 'EdenEast/nightfox.nvim' },
+    'folke/lazydev.nvim',
     opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'nightfox',
-        component_separators = '|',
-        section_separators = '',
+      library = {
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
       },
-      sections = {
-        lualine_c = {
-          { 'filename', file_status = true, newfile_status = true, path = 1 },
+    },
+    dependencies = { 'Bilal2453/luvit-meta' },
+    },
+    {
+      'neovim/nvim-lspconfig',
+      event = 'BufReadPre',
+      dependencies = {
+        { 'williamboman/mason.nvim', config = true },
+        'williamboman/mason-lspconfig.nvim',
+        { 'j-hui/fidget.nvim', event = 'LspAttach', opts = {} },
+      },
+      config = function()
+        require 'user.mason'
+      end,
+    },
+    {
+      'ray-x/lsp_signature.nvim',
+      event = 'LspAttach',
+      config = function()
+        require 'user.lsp_signature'
+      end,
+    },
+
+    -- Completion
+    {
+      'hrsh7th/nvim-cmp',
+      event = 'InsertEnter',
+      dependencies = {
+        'hrsh7th/cmp-nvim-lsp',
+        {
+          'L3MON4D3/LuaSnip',
+          dependencies = { 'rafamadriz/friendly-snippets' },
+          config = function()
+            require 'user.luasnip'
+          end,
+        },
+        'saadparwaiz1/cmp_luasnip',
+      },
+      config = function()
+        require 'user.nvim-cmp'
+      end,
+    },
+
+    -- Themes
+    {
+      'EdenEast/nightfox.nvim',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        require('nightfox').setup {}
+        vim.o.background = 'dark'
+        vim.cmd.colorscheme 'nightfox'
+      end,
+    },
+    {
+      'catppuccin/nvim',
+      name = 'catppuccin',
+      lazy = true,
+      config = function()
+        require('catppuccin').setup {
+          background = { light = 'latte', dark = 'mocha' },
+        }
+      end,
+    },
+
+    -- UI
+    {
+      'nvim-lualine/lualine.nvim',
+      event = 'VeryLazy',
+      dependencies = { 'nvim-tree/nvim-web-devicons', 'EdenEast/nightfox.nvim' },
+      opts = {
+        options = {
+          icons_enabled = true,
+          theme = 'nightfox',
+          component_separators = '|',
+          section_separators = '',
+        },
+        sections = {
+          lualine_c = {
+            { 'filename', file_status = true, newfile_status = true, path = 1 },
+          },
         },
       },
     },
-  },
-  {
-    'romgrk/barbar.nvim',
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    version = '^1.0.0',
-    init = function() vim.g.barbar_auto_setup = false end,
-    opts = {
-      animation = false,
-      insert_at_start = false,
-      insert_at_end = true,
+    {
+      'romgrk/barbar.nvim',
+      event = 'BufReadPre',
+      dependencies = 'nvim-tree/nvim-web-devicons',
+      version = '^1.0.0',
+      init = function() vim.g.barbar_auto_setup = false end,
+      opts = {
+        animation = false,
+        insert_at_start = false,
+        insert_at_end = true,
+      },
     },
-  },
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    opts = { indent = { char = '┊' } },
-  },
+    {
+      'lukas-reineke/indent-blankline.nvim',
+      event = 'BufReadPost',
+      main = 'ibl',
+      opts = { indent = { char = '┊' } },
+    },
 
-  -- File tree
-  {
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-  },
+    -- File tree
+    {
+      'nvim-tree/nvim-tree.lua',
+      version = '*',
+      cmd = { 'NvimTreeToggle', 'NvimTreeFocus', 'NvimTreeFindFile' },
+      keys = {
+        { '<leader>tt', '<cmd>NvimTreeToggle<cr>', silent = true, desc = 'Toggle NvimTree' },
+      },
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      config = function()
+        require 'user.nvim-tree'
+      end,
+    },
 
-  -- Language-specific
-  {
-    'fatih/vim-go',
-    build = ':GoUpdateBinaries',
+    -- Language-specific
+    {
+      'fatih/vim-go',
+      ft = { 'go' },
+      build = ':GoUpdateBinaries',
+      config = function()
+        vim.g.go_gopls_enabled = 0
+        vim.g.go_def_mapping_enabled = 0
+        vim.g.go_doc_keywordprg_enabled = 0
+      end,
+    },
+    {
+      'iamcco/markdown-preview.nvim',
+      ft = { 'markdown' },
+      build = function()
+        vim.fn['mkdp#util#install']()
+      end,
+    },
+
+    -- Telescope / Fuzzy finding
+    {
+      'nvim-telescope/telescope.nvim',
+      version = '*',
+      cmd = 'Telescope',
+      keys = {
+        { '<leader>ff', '<cmd>Telescope find_files<cr>',   desc = '[F]ind [F]iles' },
+        { '<leader>fg', '<cmd>Telescope live_grep<cr>',    desc = '[F]ind by [G]rep' },
+        { '<leader>fw', '<cmd>Telescope grep_string<cr>',  desc = '[F]ind current [W]ord' },
+      { '<leader>fb', '<cmd>Telescope buffers<cr>',      desc = '[F]ind [B]uffers' },
+      { '<leader>fh', '<cmd>Telescope help_tags<cr>',    desc = '[F]ind [H]elp' },
+      { '<leader>fd', '<cmd>Telescope diagnostics<cr>',  desc = '[F]ind [D]iagnostics' },
+      { '<leader>fr', '<cmd>Telescope lsp_references<cr>', desc = '[F]ind [R]eferences' },
+      { '<leader>?',  '<cmd>Telescope oldfiles<cr>',     desc = '[?] Find recently opened files' },
+      { '<leader><space>', '<cmd>Telescope buffers<cr>', desc = '[ ] Find existing buffers' },
+      { '<leader>/',  function()
+        require('telescope.builtin').current_buffer_fuzzy_find(
+          require('telescope.themes').get_dropdown { winblend = 10, previewer = false }
+        )
+      end, desc = '[/] Fuzzily search in current buffer' },
+    },
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      vim.g.go_def_mode = 'gopls'
+      require 'user.telescope'
     end,
   },
-  {
-    'iamcco/markdown-preview.nvim',
-    ft = { 'markdown' },
-    build = function()
-      vim.fn['mkdp#util#install']()
-    end,
-  },
-
-  -- Telescope / Fuzzy finding
-  { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
   {
     'nvim-telescope/telescope-fzf-native.nvim',
     build = 'make',
@@ -234,16 +297,10 @@ require('lazy').setup({
     end,
   },
 
-  -- Treesitter
-  {
-    'nvim-treesitter/nvim-treesitter',
-    branch = 'main',
-    build = ':TSUpdate',
-  },
-
   -- PlatformIO
   {
     'anurag3301/nvim-platformio.lua',
+    cmd = { 'Pioinit', 'Piorun', 'Piolib', 'Piomon', 'Piodebug' },
     cond = function()
       return vim.fn.executable('pio') == 1
     end,
@@ -253,12 +310,17 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
       { 'nvim-lua/plenary.nvim' },
       { 'folke/which-key.nvim' },
-      { 'nvim-treesitter/nvim-treesitter' },
     },
   },
 
   { import = 'custom.plugins' },
 
-}, {
-  checker = { enabled = true, notify = true, frequency = 86400 },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        'gzip', 'matchit', 'matchparen', 'netrwPlugin',
+        'tarPlugin', 'tohtml', 'tutor', 'zipPlugin',
+      },
+    },
+  },
 })
