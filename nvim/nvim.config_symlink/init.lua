50d7ab25f51998ed5580a0500d4c153d291037da
Christian <christian@stadach.com>
2026-07-15 15:12:53 +0300

    fix(init): guard nvim-tree VimEnter autocmd against non-string argv

--- nvim/nvim.config_symlink/init.lua
+++ nvim/nvim.config_symlink/init.lua
@@ -13,7 +13,7 @@ require "user.keymaps"
 vim.api.nvim_create_autocmd('VimEnter', {
   callback = function()
     local arg = vim.fn.argv(0)
-    if arg == '' or vim.fn.isdirectory(arg) == 1 then
+    if type(arg) == 'string' and (arg == '' or vim.fn.isdirectory(arg) == 1) then
       require('nvim-tree.api').tree.open()
     end
   end,
